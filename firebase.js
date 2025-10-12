913964c60d0e7ff32159ce7ae79857bab6c93949
Hafsa Mohammed Imran <hafsamohammed897@gmail.com>
2025-10-12 14:03:38 +0530

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,20 @@
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC-YKhMYK3GdknDo4rjD0ksmdlWNX6ESo8",
   authDomain: "gas-management-system-f4344.firebaseapp.com",
   projectId: "gas-management-system-f4344",
-  storageBucket: "gas-management-system-f4344.firebasestorage.app",
+  storageBucket: "gas-management-system-f4344.appspot.com",
   messagingSenderId: "511571836469",
   appId: "1:511571836469:web:91aca14dc4c99d4c51fb7c",
   measurementId: "G-CHTZ3F8BXR"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export { collection, addDoc };
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { app, db, auth };
